64b93d3c4e9bdbf5a9f740326e745a3f1a26b209
crazysrtguy <bigcharlie511@yahoo.com>
2025-05-24 13:03:03 -0400

    1111

---
+++ api/swars/daily-bonus.js
@@ -0,0 +1,6 @@
+// Daily bonus API endpoint - matches frontend expectation
+// This is a simple redirect to the actual claim-daily logic
+
+const claimDailyHandler = require('./claim-daily.js');
+
+module.exports = claimDailyHandler;
